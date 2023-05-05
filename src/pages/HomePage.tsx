3dd958fc9451b960e33f45ef54dfa19c423ce395
Jerud Ocampo <ocampo.jerud.2001@gmail.com>
2023-05-06 03:15:14 +0800

    - vite (react-ts) project initialization - tailwind initialization

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to the home page!</h1>
+            <p>This is the home page of my app.</p>
+        </div>
+    )
+}
+
+export default HomePage
